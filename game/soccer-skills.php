4983ade6f7fcf9bca29b4f36bba1d432391dea06
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2023-06-04 11:22:18 +0100

    php

---
+++ game/soccer-skills.php
@@ -0,0 +1,11 @@
+<?php
+
+$title = "Play Soccer Skills on GoneRogue";
+$description = "In Soccer Skills, tep into the shoes of a orc warrior in this thrilling adventure. Use your axe to get through to the final level.";
+$game = "Soccer Skills";
+$long_desc = "None yet!\nHi!";
+$code = "<iframe src='/soccer.html' title='Soccer Skills' frameborder='0' style='border: none;'></iframe>";
+
+require './game.php';
+
+?>
