711c7bfd9cd7e0b7fa86975b02a89ad92a41c341
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2023-06-04 11:54:44 +0100

    new

--- game/choppy-orc.php
+++ game/choppy-orc.php
@@ -4,7 +4,7 @@ $title = "Play Choppy Orc on GoneRogue";
 $description = "In Choppy Orc, tep into the shoes of a orc warrior in this thrilling adventure. Use your axe to get through to the final level.";
 $game = "Choppy Orc";
 $long_desc = "None yet!\nHi!";
-$code = "<script>document.querySelector('.game-container').style.aspectRatio = '16 / 9';</script><iframe src='/choppy-orc/index.html' title='Choppy Orc' frameborder='0' style='border: none;'></iframe>";
+$code = "<script>document.querySelector('.game-container').style.aspectRatio = '16 / 9';</script><iframe src='/choppy-orc/choppy-orc.html' title='Choppy Orc' frameborder='0' style='border: none;'></iframe>";
 
 require './game.php';
 
