05165373a166a90e38fe75f622189beb607ee7ea
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2023-06-23 19:56:28 +0100

    kpr

---
+++ game/car-drawing.php
@@ -0,0 +1,11 @@
+<?php
+
+$title = "Play Car Drawing Game on GoneRogue";
+$description = "In Car Drawing Game, tep into the shoes of a orc warrior in this thrilling adventure. Use your axe to get through to the final level.";
+$game = "Car Drawing Game";
+$long_desc = "None yet!\nHi!";
+$code = "<style>.full-screen { display: none!important; }</style><script defer>document.querySelector('.game-container').style.aspectRatio = '16 / 9';</script><iframe src='https://drawing.gonerogue.ml' title='Car Drawing Game' frameborder='0' style='border: none;'></iframe>";
+
+require './game.php';
+
+?>
