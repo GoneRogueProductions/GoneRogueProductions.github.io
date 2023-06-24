85fcfaa4eb5d5030749485f38b78cd8f2f115df9
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2023-06-24 16:39:54 +0100

    joe

--- game/happy-wheels.php
+++ game/happy-wheels.php
@@ -4,7 +4,7 @@ $title = "Play Happy Wheels on GoneRogue";
 $description = "A very happy driving game with very realistic graphics. Best for family-friendly activites. Very happy!";
 $game = "Happy Wheels";
 $long_desc = "None yet!\nHi!";
-$code = "<iframe src='/happy-wheels/index.html' title='Happy Wheels' frameborder='0' style='border: none;'></iframe>";
+$code = "<script defer>document.querySelector('.game-container').style.aspectRatio = '16 / 9';</script><iframe src='/happy-wheels/index.html' title='Happy Wheels' frameborder='0' style='border: none;'></iframe>";
 
 require './game.php';
 
