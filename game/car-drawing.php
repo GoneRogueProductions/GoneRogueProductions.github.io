4268c3a043aad1b194cc77f0ca08dfdd90c6898e
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2023-06-24 07:54:58 +0100

    fj

--- game/car-drawing.php
+++ game/car-drawing.php
@@ -3,7 +3,20 @@
 $title = "Play Car Drawing Game on GoneRogue";
 $description = "Use your artistic skills to dominate in Car Drawing Game. Try to reach the diamond and then get a gigantic speed boost.";
 $game = "Car Drawing Game";
-$long_desc = "None yet!\nHi!";
+$long_desc = "Car Drawing Game is an exciting and creative online activity that allows players to unleash their imagination and design unique cars to pass levels. This addictive game combines the thrill of artistic expression with the fun of competition.
+\n
+<b>Controls:</b> None. Use your mouse to draw the car.
+\n
+In Car Drawing Game, players are presented with a blank canvas and a wide array of tools at their disposal. Using their mouse or touchscreen, they can draw their cars with precision and detail.
+
+\n
+Once the artwork is complete, players try to beat the level whilst not being caught by the obstacles. Aim for the diamond, it gives you extra speed!
+
+\n
+Car Drawing Game offers a user-friendly interface and intuitive controls, making it accessible to players of all ages and skill levels. Whether you're a master car-drawer or a beginner, this game is for all people.
+
+\n
+So, if you have a passion for cars and a knack for drawing, get ready to rev up your artistic skills and dive into the exciting world of Car Drawing Game. Show off your talent, compete with friends, and enjoy hours of endless fun as you design the car of your dreams.";
 $code = "<style>.full-screen { display: none!important; } .game-container { max-width: 950px; width: 950px;  }</style><script defer>document.querySelector('.game-container').style.aspectRatio = '950 / 600';</script><iframe src='https://drawing.gonerogue.ml' title='Car Drawing Game' frameborder='0' style='border: none;'></iframe>";
 
 require './game.php';
--- game/choppy-orc.php
+++ game/choppy-orc.php
@@ -3,7 +3,11 @@
 $title = "Play Choppy Orc on GoneRogue";
 $description = "In Choppy Orc, tep into the shoes of a orc warrior in this thrilling adventure. Use your axe to get through to the final level.";
 $game = "Choppy Orc";
-$long_desc = "None yet!\nHi!";
+$long_desc = "<b>Controls:</b> Move the Orc with your arrow keys. Jump with the up arrow key. Press space to release his axe.\nChoppy Orc is an exhilarating 2D precision/puzzle platformer that immerses players in an extraordinary adventure as they step into the stout shoes of a valiant Orc.\n
+This captivating game transports players into the heart of a perilous journey, fraught with danger and teeming with monstrous creatures, as they strive to escape treacherous caves and valiantly rescue their captured comrades. With its charmingly retro aesthetics, Choppy Orc evokes a nostalgic sense of gaming while delivering a fresh and engrossing experience.\n
+As players traverse the intricately designed levels, they are confronted with a myriad of challenges that demand precise movements and quick thinking. The pixel-perfect controls empower players to navigate treacherous platforms, avoid menacing traps, and engage in thrilling combat encounters against the terrifying denizens of the caves. The mesmerizing world of Choppy Orc pulsates with vibrant colors, richly detailed environments, and meticulously crafted animations.\n
+Each level presents a unique puzzle, requiring players to strategically utilize their wits and agility to progress. Whether it's manipulating levers to open hidden passages, timing jumps with impeccable precision, or solving intricate riddles, every step forward in the game is a testament to the player's skill and perseverance.\n
+As the epic narrative unfolds, players uncover the plight of their captured companions, further fueling their determination to overcome any obstacle that stands in their way. The deep sense of camaraderie and heroism permeates the game, adding an emotional depth that resonates with players on their courageous quest. Choppy Orc also boasts a captivating soundtrack that harmoniously complements the on-screen action, heightening the tension and immersion. The melodic tunes intertwine with the gameplay, enhancing the overall experience and creating an unforgettable journey for players to embark upon.";
 $code = "<script defer>document.querySelector('.game-container').style.aspectRatio = '16 / 9';</script><iframe src='https://orc.gonerogue.ml/' title='Choppy Orc' frameborder='0' style='border: none;'></iframe>";
 
 require './game.php';
