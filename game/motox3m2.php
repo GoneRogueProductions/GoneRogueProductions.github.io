bed46ea49bcb62455786cc40d05c175af30f29a1
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2023-06-24 16:14:53 +0100

    joe

---
+++ game/motox3m2.php
@@ -0,0 +1,11 @@
+<?php
+
+$title = "Play MotoX3M2 on GoneRogue";
+$description = "Ride your imaginary motorbike and show off your stunts in MotoX3M2. With lots of unique obstacles and thrilling peaks, dominate, but watch out for the traps!";
+$game = "MotoX3M2";
+$long_desc = "None yet!\nHi!";
+$code = "<script defer>document.querySelector('.game-container').style.aspectRatio = '3 / 2';</script><iframe src='/motox3m2/index.html' title='MotoX3M 2' frameborder='0' style='border: none;'></iframe>";
+
+require './game.php';
+
+?>
