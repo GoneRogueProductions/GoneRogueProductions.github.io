d1e8160c4e81f26bdd9c2c41d47ad218ccfe17ca
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2023-06-24 15:28:48 +0100

    h

---
+++ termsandconditions.php
@@ -0,0 +1,463 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4WR46690Y7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4WR46690Y7');
+</script>
+    <title>GoneRogue Games: the fullscreen unblocked games site</title>
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="white">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4739296020319725"
+        crossorigin="anonymous"></script>
+    <link rel="canonical" href="https://gonerogue.ml/" />
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+
+    <meta charset="utf-8">
+    <meta name="description"
+        content="Experience gaming without interruptions at GoneRogue: your ultimate destination for immersive fullscreen games. Explore a vast collection of ad-free titles, from action-packed adventures to mind-bending puzzles.">
+    <meta name="keywords" content="gonerogue, gonerogue games, gonerogue productions, games, unblocked">
+    <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta itemprop="name" content="GoneRogue Games">
+    <meta itemprop="description"
+        content="Experience gaming without interruptions at GoneRogue: your ultimate destination for immersive fullscreen games. Explore a vast collection of ad-free titles, from action-packed adventures to mind-bending puzzles.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Social Media -->
+    <meta name="og:description"
+        content="Experience gaming without interruptions at GoneRogue: your ultimate destination for immersive fullscreen games. Explore a vast collection of ad-free titles, from action-packed adventures to mind-bending puzzles.">
+    <meta name="og:title" content="GoneRogue Games: the no-ads fullscreen games site">
+    <meta name="og:type" content="website">
+    <meta name="og:image" content="/social.png">
+    <meta name="og:url" content="https://gonerogue.ml">
+    <meta name="og:image:alt" content="GoneRogue's social cover, showing some of our games">
+
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:image" content="/social.png">
+
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="apple-mobile-web-app-title" content="GoneRogue">
+    <meta name="application-name" content="GoneRogue">
+    <meta name="msapplication-TileColor" content="#ffc40d">
+    <meta name="theme-color" content="#ffffff">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.8/umd/popper.min.js"></script>
+    <script defer src="./fontawesome.js" crossorigin="anonymous"></script>
+    <style>
+        :root {
+            --tw-ring-offset-shadow: 0 0 #0000;
+            --tw-ring-shadow: 0 0 #0000;
+            --tw-shadow: inset 0 1px 4px 0 rgba(0, 0, 0, .05);
+        }
+
+
+        @font-face {
+            font-family: Optimistic;
+            src: url(./Optimistic_Display_W_Md.woff2);
+            font-weight: normal;
+        }
+
+        @font-face {
+            font-family: Optimistic;
+            src: url(./Optimistic_Display_W_Bd.woff2);
+            font-weight: bold;
+        }
+
+        html,
+        body {
+            height: 100%;
+            width: 100%;
+        }
+
+        body,
+        div,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        a,
+        p,
+        b,
+        em {
+            font-family: Optimistic, -apple-system, ui-sans-serif, system-ui, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+        }
+
+        .navbar {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            padding: 25px;
+            margin: 0;
+            background: white;
+            border-bottom: 1px solid lightgray;
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 25px;
+            z-index: 99999;
+            align-items: center;
+            gap: 20px;
+        }
+
+        .search {
+            background: rgb(238, 238, 238);
+            display: flex;
+            padding: 10px;
+            box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+            border-radius: 10px;
+            border: 1px solid lightgray;
+            align-items: center;
+        }
+
+        .search-input {
+            background: transparent;
+            outline: none;
+            color: black;
+            box-shadow: none;
+            border: 0;
+            padding: 5px;
+            padding-left: 10px;
+        }
+
+        .search-icon {
+            padding: 5px;
+            border-right: 1px solid lightgray;
+            padding-right: 10px;
+        }
+
+        .container {
+            text-align: center;
+            margin-top: 150px;
+        }
+
+        .catalog {
+            display: grid;
+            gap: 10px;
+            grid-auto-flow: row;
+            grid-template-columns: auto auto auto;
+            margin-top: 10px;
+        }
+
+        @media (max-width: 768px) {
+            .catalog {
+                grid-template-columns: auto auto;
+            }
+        }
+
+        @media (max-width: 500px) {
+            .catalog {
+                grid-template-columns: auto;
+            }
+
+            .mobile {
+                display: inline-block !important;
+            }
+        }
+
+        .catalog div {
+            background: white;
+            border: 1px solid lightgray;
+            border-radius: 25px;
+            display: flex;
+            flex-direction: column;
+            transition: 0.1s;
+        }
+
+        /* .catalog div:hover {
+                box-shadow: 0 6px 8px 0 rgba(0, 0, 0, 0.12), 0 9px 25px 0 rgba(0, 0, 0, 0.1);
+            }
+            .catalog div:active {
+                box-shadow: none;
+            } */
+        .catalog div img {
+            width: 100%;
+            object-fit: cover;
+            border-radius: 25px 25px 0 0;
+        }
+
+        .catalog div section {
+            text-align: left;
+            padding: 25px;
+
+        }
+
+        .catalog div section span {
+            color: lightgray;
+            display: block;
+        }
+
+        @media (max-width: 600px) {
+            .navbar {
+                justify-content: center;
+            }
+        }
+
+        .tray {
+            justify-content: space-between;
+            display: flex;
+        }
+
+        .catalog div section tray aside a {
+            text-decoration: none;
+        }
+
+        .mobile {
+            display: none;
+        }
+
+        .note {
+            background: rgb(255, 238, 0);
+            border-left: 10px solid yellow;
+            color: black !important;
+            padding: 10px;
+            padding-left: 20px;
+            border-radius: 10px;
+        }
+
+        * {
+            transition: 0.4s;
+        }
+
+        @media (prefers-color-scheme: dark) {
+            body {
+                background: rgb(35, 39, 47);
+                color: rgb(235, 236, 240);
+            }
+
+            div.search {
+                background: rgb(52, 58, 70, 0.8);
+                border: 0;
+            }
+
+            .search input {
+                color: rgb(235, 236, 240);
+            }
+
+            .catalog div,
+            .navbar {
+                background: rgb(35, 39, 47);
+            }
+
+            footer.text-muted,
+            footer.bg-light {
+                background-color: rgb(59, 66, 79);
+                color: rgb(235, 236, 240);
+            }
+        }
+
+        .right-cont {
+            display: flex;
+            gap: 20px;
+        }
+
+        .b-dark {
+            display: flex;
+        }
+
+        .b-light {
+            display: none;
+        }
+
+        @media (prefers-color-scheme: dark) {
+            div.b-dark {
+                display: none;
+            }
+
+            div.b-light {
+                display: flex;
+            }
+        }
+
+        .b-dark,
+        .b-light {
+            justify-content: center;
+            align-items: center;
+            font-size: 2em;
+            cursor: pointer;
+            transition: 0s;
+        }
+
+        svg {
+            transition: 0s;
+        }
+
+        span,
+        p,
+        aside,
+        h2,
+        h1,
+        h3 {
+            transition: 0s;
+        }
+    </style>
+</head>
+
+<body id="darkTog">
+    <div class="navbar" id="darkTog1">
+        <h1><img src="https://gonerogue.ml/apple-touch-icon.png" style="border-radius: 10px; height: 75px;"
+                alt="GoneRogue Favicon"> Gаmes</h1>
+        <div class="right-cont">
+            <div class="b-dark" id="b-dark" onclick="switchTheme();">
+                <i class="fa-solid fa-moon"></i>
+            </div>
+            <div class="b-light" id="b-light" onclick="switchTheme();">
+                <i class="fa-solid fa-sun-bright"></i>
+            </div>
+            <div class="search" id="search">
+                <i class="fa-solid fa-magnifying-glass search-icon"></i>
+                <input class="search-input" type="text" placeholder="Search...">
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <h1>Terms and Conditions</h1>
+        <p>Welcome to GoneRogue! These terms and conditions ("Terms") govern your use of the GoneRogue website, including any
+        associated services, features, and content (collectively referred to as the "Platform"). By accessing or using the
+        Platform, you agree to be bound by these Terms. If you do not agree with any provision of these Terms, you may not
+        access or use the Platform.
+        
+        Content:
+        a. User-Generated Content: The Platform may allow users to submit or upload content, such as comments or reviews ("User
+        Content"). You retain ownership of your User Content, but by submitting it, you grant GoneRogue a worldwide,
+        royalty-free, perpetual, irrevocable, sublicensable, and transferable license to use, reproduce, distribute, modify,
+        adapt, publicly display, and perform your User Content in connection with the operation and promotion of the Platform.
+        b. Prohibited Content: You agree not to submit or upload any User Content that is unlawful, offensive, defamatory, or
+        infringing upon the rights of others.
+        Use of the Platform:
+        a. Compliance: You agree to comply with all applicable laws, regulations, and these Terms while using the Platform.
+        b. Prohibited Conduct: You agree not to engage in any conduct that may:
+        i. Violate any laws or regulations.
+        ii. Infringe upon the rights of GoneRogue or any third party.
+        iii. Interfere with the operation or security of the Platform.
+        iv. Access or attempt to access any unauthorized areas of the Platform.
+        v. Transmit any viruses, malware, or other harmful code.
+        Third-Party Links:
+        The Platform may contain links to third-party websites or services that are not owned or controlled by GoneRogue.
+        GoneRogue is not responsible for the content or privacy practices of any third-party websites or services. Your use of
+        such websites or services is at your own risk.
+        Disclaimer of Warranties:
+        a. The Platform is provided on an "as is" and "as available" basis. GoneRogue makes no representations or warranties of
+        any kind, express or implied, regarding the accuracy, reliability, or availability of the Platform.
+        b. GoneRogue disclaims all warranties, whether express or implied, including but not limited to warranties of
+        merchantability, fitness for a particular purpose, and non-infringement.
+        Limitation of Liability:
+        a. In no event shall GoneRogue or its affiliates, officers, directors, employees, or licensors be liable for any
+        indirect, consequential, incidental, special, or punitive damages arising out of or in connection with your use of the
+        Platform.
+        b. GoneRogue's total liability for any claim arising under these Terms shall not exceed the amount paid by you, if any,
+        for accessing the Platform.
+        Termination:
+        GoneRogue may terminate or suspend your access to the Platform at any time, without notice or liability, for any reason.
+        Upon termination, these Terms will continue to apply to any remaining obligations or restrictions.
+        General:
+        a. Entire Agreement: These Terms constitute the entire agreement between you and GoneRogue regarding the Platform and
+        supersede any prior agreements or understandings.
+        b. Governing Law: These Terms shall be governed by and construed in accordance with the laws of [Your jurisdiction].
+        c. Severability: If any provision of these Terms is found to be invalid or unenforceable, the remaining provisions shall
+        remain in full force and effect.
+        d. Waiver: The failure of GoneRogue to enforce any right or provision of these Terms shall not constitute a waiver of
+        such right or provision.
+        By accessing and using the Platform, you acknowledge that you have read, understood, and agree to be bound by these
+        Terms and any additional terms and conditions provided within the Platform.</p>
+    </div>
+
+    <!-- Footer -->
+    <footer class="text-center text-lg-start bg-light text-muted" id="footer"
+        style="padding-top: 25px; margin-top: 25px;">
+        <!-- Section: Social media -->
+
+        <!-- Section: Links  -->
+        <section class="">
+            <div class="container text-center text-md-start mt-5">
+                <!-- Grid row -->
+                <div class="row mt-3">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <!-- Content -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            GoneRogue Gаmes
+                        </h6>
+                        <p>
+                            GoneRogue has a library of gаmes above, but it seems like you've scrolled to the end. We're
+                            working on requesting gаmes.
+                        </p>
+                    </div>
+                    </div>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+
+        </section>
+        <!-- Section: Links  -->
+        <a herf="/termsandconditions.html">T&Cs</a>
+    </footer>
+    <!-- Footer -->
+    <script>
+        // Dark mode toggle
+        let theme = true;
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            let theme = false;
+        }
+        function switchTheme() {
+            if (theme === true) {
+                document.getElementById("b-dark").style.display = "none";
+                document.getElementById("b-light").style.display = "flex";
+                document.getElementById("darkTog").style.background = "rgb(35,39,47)";
+                document.getElementById("darkTog").style.color = "rgb(235, 236, 240)";
+                document.getElementById("darkTog1").style.background = "rgb(35,39,47)";
+                document.getElementById("darkTog1").style.color = "rgb(235, 236, 240)";
+                document.getElementById("search").style.background = "rgb(52, 58, 70,0.8)";
+                document.getElementById("search").style.border = "0";
+                document.getElementById("search").style.color = "rgb(235, 236, 240)";
+                document.getElementById("footer").style.backgroundColor = "rgb(59, 66, 79)!important";
+                document.getElementById("footer").style.color = "rgb(235, 236, 240)!important";
+                $('#catalog').children('div').each(function () {
+                    this.style.background = "rgb(35,39,47)";
+                });
+                theme = false;
+            }
+            else if (theme === false) {
+                document.getElementById("b-light").style.display = "none";
+                document.getElementById("b-dark").style.display = "flex";
+                document.getElementById("darkTog").style.background = "white";
+                document.getElementById("darkTog").style.color = "black";
+                document.getElementById("darkTog1").style.background = "white";
+                document.getElementById("darkTog1").style.color = "black";
+                document.getElementById("search").style.background = "rgb(238, 238, 238)";
+                document.getElementById("search").style.border = "0";
+                document.getElementById("search").style.color = "black";
+                document.getElementById("footer").style.background = "white";
+                document.getElementById("footer").style.color = "black";
+                $('#catalog').children('div').each(function () {
+                    this.style.background = "white";
+                });
+                theme = true;
+            }
+
+
+        }
+    </script>
+</body>
+
+</html>
