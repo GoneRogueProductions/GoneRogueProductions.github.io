f4f6445f6253aa566ff6542b4fe5df254c4051c8
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2023-06-24 14:30:49 +0100

    joe

--- game/slope.php
+++ game/slope.php
@@ -1,7 +1,7 @@
 <?php
 
 $title = "Play Slope on GoneRogue";
-$description = "Slope. Defy gravity and master the treacherous slopes in this thrilling game of skill. Keep your ball rolling and survive the twists ans obstacles.";
+$description = "Slope. Defy gravity and master the treacherous slopes in this thrilling game of skill. Keep your ball rolling and survive the twists and obstacles.";
 $game = "Slope";
 $long_desc = "None yet!\nHi!";
 $code = "<iframe src='https://slope.gonerogue.ml/' title='Slope' frameborder='0' style='border: none;'></iframe>";
---
+++ game/table-tanks.php
@@ -0,0 +1,11 @@
+<?php
+
+$title = "Play Table Tanks on GoneRogue";
+$description = "Shoot all the enemy tanks in Table Tanks, but there is s twist. It's all on a 2D table with walls and only 3 pieces of ammo. Pick up boxes to restore it, but risk your life.";
+$game = "Table Tanks";
+$long_desc = "None yet!\nHi!";
+$code = "<script defer>document.querySelector('.game-container').style.aspectRatio = '16 / 9';</script><iframe src='/table-tanks/index.html' title='Choppy Orc' frameborder='0' style='border: none;'></iframe>";
+
+require './game.php';
+
+?>
