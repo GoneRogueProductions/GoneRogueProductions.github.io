7337f87cc3f2aef5f27962022704f56650c182b1
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2023-06-25 08:45:13 +0100

    random

--- index.php
+++ index.php
@@ -562,7 +562,7 @@ $hw = "/game/happy-wheels";
     <!-- Footer -->
     <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script defer src="./main.min.js"></script>
-    <script defer src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4739296020319725"
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4739296020319725"
         crossorigin="anonymous"></script>
         <script src="/lazysizes.min.js" async></script>
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.8/umd/popper.min.js"></script>
